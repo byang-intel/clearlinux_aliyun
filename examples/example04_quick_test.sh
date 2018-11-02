50ad71a527d34d4d0d189cc9c6d8d6aa8a17705c
Bin Yang <bin.yang@intel.com>
2018-11-02 16:57:25 +0800

    example: download img from cdn instead
    Signed-off-by: Bin Yang <bin.yang@intel.com>

--- examples/example04_quick_test.sh
+++ examples/example04_quick_test.sh
@@ -7,10 +7,10 @@ if [ ! -e packages/helloclear ]; then
 	exit 1
 fi
 
-[ -e clear.img ] || ( curl -o clear.img.xz https://download.clearlinux.org/image/clear-$(curl https://download.clearlinux.org/latest)-kvm.img.xz && unxz -v clear.img.xz )
-[ -e OVMF.fd ] || curl -O https://download.clearlinux.org/image/OVMF.fd 
+[ -e clear.img ] || ( curl -o clear.img.xz https://cdn.download.clearlinux.org/image/clear-$(curl https://cdn.download.clearlinux.org/latest)-kvm.img.xz && unxz -v clear.img.xz )
+[ -e OVMF.fd ] || curl -O https://cdn.download.clearlinux.org/image/OVMF.fd 
 if [ ! -e start_qemu.sh ]; then
-	curl -O https://download.clearlinux.org/image/start_qemu.sh
+	curl -O https://cdn.download.clearlinux.org/image/start_qemu.sh
 	if [ ! -e /dev/kvm ]; then
 		sed -i -e "s/-enable-kvm//g" -e "s/-cpu host/-cpu Haswell/g" -e "s/-smp sockets=.,cpus=.,cores=./-smp sockets=1,cpus=`nproc`,cores=`nproc`/g" start_qemu.sh
 	fi
